693c9124ed2892a9c7b197432629982e160f8d8e
KristaGoralczyk <krista.goralczyk@gmail.com>
2013-07-03 10:52:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShinyBear::Application.config.secret_token = '1bd9b0e2fa9ac608444c9d70cb751f2da9c063570ab3fc63b1c891398c73b23ebb5e3a767545df29f5d5194ca2dbb216b5dbd22a2355996389224c9d6f947e47'
